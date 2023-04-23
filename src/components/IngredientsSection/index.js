6578306073418f3f3e92aae08fe6710fd90891d6
Maksim Perevalov <skai-net1989@mail.ru>
2023-04-23 20:34:18 +0300

    FIX

--- src/components/IngredientsSection/index.js
+++ src/components/IngredientsSection/index.js
@@ -6,7 +6,7 @@ import IngredientType from '../../types/Ingredient';
 import cs from './styles.module.css';
 
 const IngredientsSection = ({ name, className, index, ingredients }) => {
-  let ingredientsEls = ingredients.map((item) => <Ingredient data={item} key={item._id} />);
+  const ingredientsEls = ingredients.map((item) => <Ingredient data={item} key={item._id} />);
   return (
     <div className={combineClasses(cs.section, className)} data-index={index}>
       <h2 className={`text text_type_main-medium mb-6 ${cs.header}`}>{name}</h2>
